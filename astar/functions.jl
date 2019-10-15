34cc65dcb4319d79eda6e8fe9575c3150c96402f
sdwhardy <40562565+sdwhardy@users.noreply.github.com>
2019-10-15 16:25:34 +0200

    astar functions

--- astar/functions.jl
+++ astar/functions.jl
@@ -2,9 +2,16 @@ function Astar(start,goal,ocn)#node
     openQ=Array{AstarNode,1}()
     closedQ=Array{AstarNode,1}()
     current=AstarNode()
-    nd.node=start
-    nd.G_cost=0
-    nd.H_cost=lof_pnt2pnt_dist(start.xy,goal.xy)
-    nd.F_cost=current.G_cost+current.H_cost
+    current.node=start
+    current.G_cost=0
+    current.H_cost=lof_pnt2pnt_dist(start.xy,goal.xy)
+    current.F_cost=current.G_cost+current.H_cost
+    as_openNode(current,openQ,goal)
+end
 
+function as_openNode(crnt,oQ,goal)
+    for edg in crnt.edges
+        if edg.openQ == false && edg.closedQ == false
+
+    end
 end
